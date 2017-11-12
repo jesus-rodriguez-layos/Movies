49273af6d1447e75ca93d37a2bb15a464eaecd09
Jesús Rodríguez <jrdev9@gmail.com>
2017-11-12 19:21:39 +0100

    Rename main activity to discover movies activity

--- app/src/main/java/com/jrdev9/movies/modules/discover/DiscoverMoviesActivity.java
+++ app/src/main/java/com/jrdev9/movies/modules/discover/DiscoverMoviesActivity.java
@@ -1,13 +1,15 @@
-package com.jrdev9.movies;
+package com.jrdev9.movies.modules.discover;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.jrdev9.movies.R;
+
+public class DiscoverMoviesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_discover_movies);
     }
 }
