6d31e27f5bf864c5e65763e6a81541e86bb99cda
Jesús Rodríguez <jrdev9@gmail.com>
2017-11-16 20:12:13 +0100

    Add module discover movies

---
+++ app/src/main/java/com/jrdev9/movies/modules/discovermovies/di/DiscoverMoviesModule.java
@@ -0,0 +1,12 @@
+package com.jrdev9.movies.modules.discovermovies.di;
+
+
+import com.jrdev9.movies.app.di.modules.BaseActivityModule;
+import com.jrdev9.movies.modules.discovermovies.presentation.DiscoverMoviesActivity;
+
+import dagger.Module;
+
+@Module
+public class DiscoverMoviesModule extends BaseActivityModule<DiscoverMoviesActivity> {
+
+}
