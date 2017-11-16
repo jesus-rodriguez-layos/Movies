13d25e146fe443fa6b375147300b6e2443a37fbd
Jesús Rodríguez <jrdev9@gmail.com>
2017-11-16 20:13:45 +0100

    Add view discover movies

---
+++ app/src/main/java/com/jrdev9/movies/modules/discovermovies/presentation/DiscoverMoviesView.java
@@ -0,0 +1,10 @@
+package com.jrdev9.movies.modules.discovermovies.presentation;
+
+import com.jrdev9.movies.app.presentation.base.BaseView;
+
+import me.panavtec.threaddecoratedview.views.qualifiers.ThreadDecoratedView;
+
+@ThreadDecoratedView
+public interface DiscoverMoviesView extends BaseView {
+
+}
