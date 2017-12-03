3acc2154cf51a47ccc1a55c28dfd068ec73261b7
Jesús Rodríguez <jrdev9@gmail.com>
2017-12-03 18:49:20 +0100

    Replace query map to query when call discover movies

--- app/src/main/java/com/jrdev9/movies/app/data/api/ApiTheMovieDatabaseEndPoint.java
+++ app/src/main/java/com/jrdev9/movies/app/data/api/ApiTheMovieDatabaseEndPoint.java
@@ -4,11 +4,9 @@ package com.jrdev9.movies.app.data.api;
 import com.jrdev9.movies.BuildConfig;
 import com.jrdev9.movies.modules.discovermovies.data.sources.api.models.ApiDiscoverMoviesResponse;
 
-import java.util.Map;
-
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.QueryMap;
+import retrofit2.http.Query;
 
 
 public interface ApiTheMovieDatabaseEndPoint {
@@ -18,5 +16,5 @@ public interface ApiTheMovieDatabaseEndPoint {
     String API_KEY = BuildConfig.THE_MOVIE_DB_API_KEY;
 
     @GET("discover/movie")
-    Call<ApiDiscoverMoviesResponse> discoverMovies(@QueryMap Map<String, String> options);
+    Call<ApiDiscoverMoviesResponse> discoverMovies(@Query("api_key") String apiKey);
 }
